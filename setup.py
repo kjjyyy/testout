8240101be292f49834bb62d94867a8ae9d1090d7
김정윤 <97427304+kjjyyy@users.noreply.github.com>
2022-05-04 11:17:51 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup (
+  include_package_data=True,
+  name = calc
+  version = '0.0.1',
+  description = 
+  author='kimjy'
+  author_email='asdf7352@naver.com',
+  download_url=
+  install_requires=
+  long_description=
+  long_description_content_type=
+  classifiers=[
+  ],
+)
+  
